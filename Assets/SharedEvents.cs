92c98de16e65fa4916934e77af6f2fdb427fc2bb
Denis Kozlov <deniskozlov@hotmail.com>
2019-02-03 15:30:44 +0400

    Init

---
+++ Assets/SharedEvents.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharedEvents : MonoBehaviour
+{
+    //Хранилище подписок и делегатов на их обработчики
+    private readonly Dictionary<string, List<Delegate>> _subscribers = new Dictionary<string, List<Delegate>>();
+
+    //Подписывает на события с названием eventName
+    public void Subscribe<T>(string eventName, Action<T> callback) where T : EventData
+    {
+        if (!_subscribers.ContainsKey(eventName))
+        {
+            var listOfDelegates = new List<Delegate>();
+            _subscribers.Add(eventName, listOfDelegates);
+        }
+        _subscribers[eventName].Add(callback);
+    }
+
+    //Отписывает от события с названием eventName
+    public void Unsubscribe<T>(string eventName, Action<T> callback) where T : EventData
+    {
+        if (_subscribers.ContainsKey(eventName))
+        {
+            var listOfDelegates = _subscribers[eventName];
+            listOfDelegates.Remove(callback);
+        }
+    }
+
+    //Оправка данных data подписчикам на событие eventName
+    public void Publish<T>(string eventName, T data) where T : EventData
+    {
+        if (_subscribers.ContainsKey(eventName))
+        {
+            var listOfDelegates = _subscribers[eventName];
+
+            foreach (Action<T> callback in listOfDelegates)
+            {
+                callback(data);
+            }
+        }
+    }
+}
+
+//Базовый класс для данных, передаваемых через подписки-публикации
+public abstract class EventData
+{
+    //Инициатор события (публикатор)
+    public MonoBehaviour Sender { get; set; }
+}
---
+++ Assets/SharedState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SharedEvents))]
+public class SharedState : MonoBehaviour
+{
+    //Закрытый словарь, который будет содержать все данные
+    private Dictionary<string, object> _data = new Dictionary<string, object>();
+
+    private SharedEvents _sharedEvents;
+
+    void Start()
+    {
+        _sharedEvents = GetComponent<SharedEvents>();
+    }
+
+
+    //Индексатор для доступа к данным
+    public object this[string key]
+    {
+        get { return _data[key]; }
+        set
+        {
+            _data[key] = value;
+            _sharedEvents.Publish("sharedstatechanged",
+                new SharedStateChangedEventData { Sender = this, Field = key, NewValue = value });
+        }
+    }
+
+}
+
+
+public class SharedStateChangedEventData : EventData
+{
+    //Название параметра в общем состоянии
+    public string Field { get; set; }
+
+    //Новое значение параметра (изменение в состоянии)
+    public object NewValue { get; set; }
+}
